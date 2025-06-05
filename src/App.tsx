ab4e822a79c98f98038658f2f8d49a93d0820fcb
daliaalawneh <alawnehdalia340@gmail.com>
2025-06-05 10:02:01 +0300

    Create mui theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import Home from "./areas/user/pages/Home";
+import theme from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 function App() {
   return (
-    <>
-
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Home />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/theme/index.ts
@@ -0,0 +1,62 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#000000",
+    },
+    secondary: {
+      main: "#FEFA17",
+    },
+    background: {
+      default: "#ffffff",
+      paper: "#f9f9f9",
+    },
+    text: {
+      primary: "#111111",
+      secondary: "#555555",
+    },
+  },
+  typography: {
+    fontFamily: '"Manrope", sans-serif',
+    h1: {
+      fontSize: "4rem",
+      fontWeight: 900,
+    },
+    h2: {
+      fontSize: "3.25rem",
+      fontWeight: 900,
+    },
+    h3: {
+      fontSize: "1.5rem",
+      fontWeight: 900,
+    },
+    body1: {
+      fontSize: "1.25rem",
+      fontWeight: 500,
+    },
+    body2: {
+      fontSize: "1rem",
+      fontWeight: 500,
+    },
+  },
+  shape: {
+    borderRadius: 16,
+  },
+  components: {
+    MuiButton: {
+      defaultProps: {
+        variant: "contained",
+      },
+      styleOverrides: {
+        root: {
+          borderRadius: 9999,
+          padding: "13px 23px",
+        },
+      },
+    },
+  },
+});
+
+export default theme;
