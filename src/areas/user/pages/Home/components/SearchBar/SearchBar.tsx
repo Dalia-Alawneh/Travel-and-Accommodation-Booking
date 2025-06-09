a423b6cd8707746e5f8d74e6ebb89c9b66f8b174
daliaalawneh <alawenehdalia@gmail.com>
2025-06-09 08:34:49 +0300

    refactor: extract paper style

--- src/areas/user/pages/Home/components/SearchBar/SearchBar.tsx
+++ src/areas/user/pages/Home/components/SearchBar/SearchBar.tsx
@@ -1,9 +1,20 @@
-import { Paper, SelectChangeEvent } from "@mui/material";
+import { Box, Divider, Paper, SelectChangeEvent } from "@mui/material";
 import AppSelect from "@travelia/components/Inputs/Select/Select";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import { cities } from "@travelia/fixtures";
 import { useState } from "react";
 import { SelectItem } from "@travelia/types";
+const paperStyle = {
+  bgcolor: "#fff",
+  position: "absolute",
+  left: "50%",
+  bottom: -50,
+  boxShadow: "0px 0px 5px -1px rgba(0, 0, 0, 0.2)",
+  transform: "translateX(-50%)",
+  width: { xs: "95%", md: "90%", lg: "75%" },
+  borderRadius: "12px",
+  p: 5,
+};
 
 const SearchBar = () => {
   const [city, setCity] = useState<SelectItem>(cities[0]);
@@ -16,26 +27,33 @@ const SearchBar = () => {
   };
 
   return (
-    <Paper
-      sx={{
-        bgcolor: "#fff",
-        position: "absolute",
-        left: "50%",
-        bottom: -50,
-        boxShadow: "0px 0px 5px -1px rgba(0, 0, 0, 0.2)",
-        transform: "translateX(-50%)",
-        width: { xs: "95%", md: "90%", lg: "75%" },
-        borderRadius: "12px",
-        p: 3,
-      }}
-    >
-      <AppSelect
-        items={cities}
-        label="Location"
-        item={city}
-        onChange={handleChange}
-        icon={<LocationOnIcon sx={{ fontSize: 18, color: "#ddd" }} />}
-      />
+    <Paper sx={paperStyle}>
+      <Box display="flex" alignItems="center" gap="20px">
+        <AppSelect
+          items={cities}
+          label="Location"
+          item={city}
+          onChange={handleChange}
+          icon={<LocationOnIcon sx={{ fontSize: 18, color: "#ddd" }} />}
+        />
+        <Divider
+          orientation="vertical"
+          flexItem
+          sx={{ height: "50px", width: "1px" }}
+        />
+        <AppSelect
+          items={cities}
+          label="Check In"
+          item={city}
+          onChange={handleChange}
+          icon={<LocationOnIcon sx={{ fontSize: 18, color: "#ddd" }} />}
+        />
+        <Divider
+          orientation="vertical"
+          flexItem
+          sx={{ height: "50px", width: "1px" }}
+        />
+      </Box>
     </Paper>
   );
 };
