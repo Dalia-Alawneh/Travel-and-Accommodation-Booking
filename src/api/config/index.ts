0373ac060985852d84e67536b35bab1d0853cef1
daliaalawneh <alawnehdalia340@gmail.com>
2025-06-12 21:24:03 +0300

    feature: Add env file

--- src/api/config/index.ts
+++ src/api/config/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://hotel.foothilltech.net",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
