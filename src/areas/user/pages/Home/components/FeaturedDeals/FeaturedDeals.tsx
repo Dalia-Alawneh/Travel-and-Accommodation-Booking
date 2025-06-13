e8344c646f6bd4730d55173d05a7d07c2ee9b54e
daliaalawneh <alawnehdalia340@gmail.com>
2025-06-13 08:57:54 +0300

    refactor: add cards skeleton for loading state

--- src/areas/user/pages/Home/components/FeaturedDeals/FeaturedDeals.tsx
+++ src/areas/user/pages/Home/components/FeaturedDeals/FeaturedDeals.tsx
@@ -3,6 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 import { getFeaturedDeals } from "@travelia/api/endpoints/home";
 import RoomCard from "@travelia/areas/user/components/RoomCard";
 import PauseOnHoverCarousel from "../PauseOnHoverCarousel/PauseOnHoverCarousel";
+import RoomCardSkeleton from "@travelia/areas/user/components/RoomCard/RoomCardSkeleton";
 
 const FeaturedDeals = () => {
   const { isLoading, data: featureDeals } = useQuery({
@@ -15,7 +16,20 @@ const FeaturedDeals = () => {
       <Typography variant="h2" mb={5}>
         Featured Deals
       </Typography>
-      {!isLoading && featureDeals?.length === 0 ? (
+      {isLoading ? (
+        <Box
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            gap: 2,
+            px: { xs: 1, sm: 2 },
+          }}
+        >
+          {Array.from({ length: 3 }, (_, index) => (
+            <RoomCardSkeleton key={index} />
+          ))}
+        </Box>
+      ) : featureDeals?.length === 0 ? (
         <Box textAlign="center" color="text.secondary">
           No data found
         </Box>
