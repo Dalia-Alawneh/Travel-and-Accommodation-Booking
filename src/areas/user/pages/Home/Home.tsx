fb10d938cf0a2cc86421a6ff2d9d1dc373d44e34
daliaalawneh <alawnehdalia340@gmail.com>
2025-06-13 09:33:55 +0300

    refactor: make `TopBar` reusable

--- src/areas/user/pages/Home/Home.tsx
+++ src/areas/user/pages/Home/Home.tsx
@@ -2,11 +2,12 @@ import { Box } from "@mui/material";
 import TopBar from "../../../../components/TopBar";
 import Header from "./components/Header";
 import FeaturedDeals from "./components/FeaturedDeals/FeaturedDeals";
+import { menuItems } from "@travelia/fixtures";
 
 const Home = () => {
   return (
     <div>
-      <TopBar />
+      <TopBar menuLinks={menuItems} />
       <Header />
       <Box py={3}></Box>
       <FeaturedDeals />
--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -10,8 +10,13 @@ import { DRAWER_WIDTH, menuItems } from "@travelia/fixtures";
 import AppDrawer from "../Drawer/Drawer";
 import AppButton from "../Button/Button";
 import { Container } from "@mui/material";
+import { Menu } from "@travelia/types";
 
-export default function TopBar() {
+interface ITopBarProps {
+  menuLinks: Menu;
+}
+
+export default function TopBar({ menuLinks }: ITopBarProps) {
   const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
 
   const handleDrawerToggle = () => {
@@ -57,7 +62,7 @@ export default function TopBar() {
                   justifyContent: "center",
                 }}
               >
-                {menuItems.map((item) => (
+                {menuLinks.map((item) => (
                   <Typography variant="body2" key={item.title}>
                     {item.title}
                   </Typography>
