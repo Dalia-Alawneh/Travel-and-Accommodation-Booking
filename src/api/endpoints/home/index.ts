716e5b97951792e3635024fdfa6e79328ed08cc8
daliaalawneh <alawnehdalia340@gmail.com>
2025-06-13 08:55:56 +0300

    refactor: add featured deals response interface

--- src/api/endpoints/home/index.ts
+++ src/api/endpoints/home/index.ts
@@ -1,7 +1,8 @@
 import Axios from "@travelia/api/config";
+import { IFeaturedDealsResponse } from "@travelia/api/types";
 
 const endpoint = "/home";
-export const getFeaturedDeals = async () => {
+export const getFeaturedDeals = async (): Promise<IFeaturedDealsResponse[]> => {
   const response = await Axios.get(`${endpoint}/featured-deals`);
   return response.data;
 };
---
+++ src/api/types/index.ts
@@ -0,0 +1,12 @@
+export interface IFeaturedDealsResponse {
+  cityName: string;
+  description: string;
+  discount: number;
+  finalPrice: number;
+  hotelId: number;
+  hotelName: string;
+  hotelStarRating: number;
+  originalRoomPrice: number;
+  roomPhotoUrl: string;
+  title: string;
+}
