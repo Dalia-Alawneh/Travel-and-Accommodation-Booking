1d86ddec9cc25d43a5b523968cb6d3ef730c74a3
daliaalawneh <alawnehdalia340@gmail.com>
2025-06-21 09:58:23 +0300

    refactor: remove duplicates components in home

--- src/areas/user/pages/Home/Home.tsx
+++ src/areas/user/pages/Home/Home.tsx
@@ -1,20 +1,15 @@
-import TopBar from "../../../../components/TopBar";
 import Header from "./components/Header";
 import FeaturedDeals from "./components/FeaturedDeals/FeaturedDeals";
-import { menuItems } from "@travelia/fixtures";
 import TrendingDestinations from "./components/TrendingDestinations/TrendingDestinations";
-import Footer from "../../components/Footer";
 import WrappedRecentlyVisited from "./components/RecentlyVisited";
 
 const Home = () => {
   return (
     <div>
-      <TopBar menuLinks={menuItems} />
       <Header />
       <FeaturedDeals />
       <WrappedRecentlyVisited />
       <TrendingDestinations />
-      <Footer />
     </div>
   );
 };
