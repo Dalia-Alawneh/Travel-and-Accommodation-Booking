5833ea3a0ab39a864a898e8b2ba0e1ebc9c753cb
daliaalawneh <alawnehdalia@gmail.com>
2025-06-25 21:14:39 +0300

    refactor: add fallback for map

--- src/areas/user/components/Map/Map.tsx
+++ src/areas/user/components/Map/Map.tsx
@@ -1,27 +1,57 @@
 import * as React from "react";
 import Map, { Marker } from "react-map-gl/mapbox";
-// If using with mapbox-gl v1:
-// import Map from 'react-map-gl/mapbox-legacy';
 import "mapbox-gl/dist/mapbox-gl.css";
+import { Box } from "@mui/material";
+import { Warning } from "@mui/icons-material";
+
 interface IMapProps {
   lat: number;
   lng: number;
 }
 
+const mapFallbackSx = {
+  width: "100%",
+  height: "100%",
+  borderRadius: 12,
+  backgroundColor: "#eee",
+  display: "flex",
+  gap: 4,
+  alignItems: "center",
+  justifyContent: "center",
+  fontSize: 16,
+};
 const AppMap = ({ lat, lng }: IMapProps) => {
+  const [mapError, setMapError] = React.useState(false);
+
   return (
-    <Map
-      mapboxAccessToken="pk.eyJ1IjoiZGFsaWFzc3MiLCJhIjoiY21jYzg2NjZuMDQ5eTJscXp5dm9zb3VjZCJ9.NtLpIT3XqyyazbNc9tUc6g"
-      initialViewState={{
-        longitude: lng,
-        latitude: lat,
-        zoom: 14,
+    <div
+      style={{
+        width: "100%",
+        height: 320,
+        borderRadius: 12,
+        position: "relative",
       }}
-      style={{ width: "100%", height: 320, borderRadius: 12 }}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
     >
-      <Marker latitude={lat} longitude={lng} />
-    </Map>
+      {!mapError ? (
+        <Map
+          mapboxAccessToken="pk.eyJ1IjoiZGFsaWFzc3MiLCJhIjoiY21jYzg2NjZuMDQ5eTJscXp5dm9zb3VjZCJ9.NtLpIT3XqyyazbNc9tUc6g"
+          initialViewState={{
+            longitude: lng,
+            latitude: lat,
+            zoom: 14,
+          }}
+          style={{ width: "100%", height: "100%", borderRadius: 12 }}
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+          onError={() => setMapError(true)}
+        >
+          <Marker latitude={lat} longitude={lng} />
+        </Map>
+      ) : (
+        <Box style={mapFallbackSx}>
+          <Warning fontSize="small" color="warning" /> Error loading map
+        </Box>
+      )}
+    </div>
   );
 };
 
