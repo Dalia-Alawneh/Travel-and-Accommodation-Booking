2cb948f436bbdbd8ff96889344506e99333b7cbd
daliaalawneh <alawnehdalia340@gmail.com>
2025-06-28 22:30:40 +0300

    fix: mock material icons

--- vitest-setup.tsx
+++ vitest-setup.tsx
@@ -4,5 +4,7 @@ import { vi } from "vitest";
 vi.mock("@mui/icons-material", async () => {
   return {
     Warning: () => <div data-testid="mock-icon" />,
+    Dashboard: () => <div data-testid="mock-icon" />,
+    Logout: () => <div data-testid="mock-icon" />,
   };
 });
