326b63341046638c3648453a378e8cda1e4eba8e
daliaalawneh <alawnehdalia340@gmail.com>
2025-06-29 08:22:00 +0300

    test: implement api mocks handlers for cities

--- src/mocks/apiMocks/cities.ts
+++ src/mocks/apiMocks/cities.ts
@@ -1,5 +1,6 @@
 import { http, HttpResponse } from "msw";
 import { citiesMockedResponse } from "./../data";
+import { ICityPayload } from "@travelia/api/types/request.dto";
 
 export const BASE_URL = "https://hotel.foothilltech.net";
 export const handlers = [
@@ -7,5 +8,28 @@ export const handlers = [
     return HttpResponse.json({
       cities: citiesMockedResponse,
     });
+  }),
+
+  http.post(`${BASE_URL}/cities`, async ({ request }) => {
+    const body = (await request.json()) as ICityPayload;
+    return HttpResponse.json(
+      {
+        message: "City added successfully",
+        note: {
+          ...body,
+          _id: "2",
+        },
+      },
+      { status: 201 },
+    );
+  }),
+
+  http.delete(`${BASE_URL}/cities/:id`, () => {
+    return HttpResponse.json({ message: "Deleted" }, { status: 200 });
+  }),
+
+  http.put(`${BASE_URL}/cities/:id`, async ({ request }) => {
+    const body = await request.json();
+    return HttpResponse.json(body, { status: 200 });
   }),
 ];
