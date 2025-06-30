009cfe0bfe219d4243376feddab8027875e9da97
daliaalawneh <alawnehdalia340@gmail.com>
2025-06-30 09:16:43 +0300

    refactor: button has variants

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,21 +1,77 @@
-import { Button, ButtonProps } from "@mui/material";
+import { Button, ButtonProps, CircularProgress, Box } from "@mui/material";
 import { ReactNode } from "react";
+
+type AppButtonType = "primary" | "secondary" | "logout";
 
 interface IAppButtonProps extends ButtonProps {
   children: ReactNode;
+  loading?: boolean;
+  appVariant?: AppButtonType;
 }
 
-const AppButton = ({ children, sx, ...rest }: IAppButtonProps) => {
+const variantStyles = {
+  primary: {
+    bgcolor: "custom.skyBlue",
+    color: "white",
+    px: 4,
+    mt: 4,
+    "&:hover": {
+      bgcolor: "custom.blueDark",
+    },
+  },
+  secondary: {
+    bgcolor: "custom.salver",
+    color: "primary.dark",
+    fontWeight: 600,
+    py: 1,
+    width: { xs: "100%", sm: "initial" },
+    "&:hover": {
+      bgcolor: "custom.orange",
+      color: "custom.beige",
+    },
+  },
+  logout: {
+    bgcolor: "transparent",
+    color: "primary.dark",
+    py: 1,
+    ml: 2,
+    display: { xs: "none", sm: "flex" },
+    textTransform: "none",
+  },
+};
+
+const AppButton = ({
+  children,
+  loading = false,
+  disabled,
+  appVariant,
+  sx,
+  ...rest
+}: IAppButtonProps) => {
+  const style = appVariant ? variantStyles[appVariant] : {};
+
   return (
     <Button
+      disabled={disabled || loading}
       sx={{
-        bgcolor: "transparent",
-        color: "primary.dark",
+        textTransform: "none",
+        ...style,
         ...sx,
       }}
       {...rest}
     >
-      {children}
+      {loading ? (
+        <CircularProgress
+          size={24}
+          sx={{ color: style.color || "primary.main" }}
+        />
+      ) : appVariant === "logout" ? (
+        <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+          {children}
+        </Box>
+      ) : (
+        children
+      )}
     </Button>
   );
 };
