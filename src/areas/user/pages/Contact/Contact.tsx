a84049bbaf81fa4b409b56aad863594c6cd076a4
daliaalawneh <daliaa@brocali.com>
2025-07-11 11:09:35 +0300

    feature: Implement email js service

--- src/areas/user/pages/Contact/Contact.tsx
+++ src/areas/user/pages/Contact/Contact.tsx
@@ -1,9 +1,13 @@
 import { Box, Button } from "@mui/material";
+import { type FormikHelpers } from "formik";
+import emailjs from "@emailjs/browser";
 import Main from "@travelia/components/MainSection";
 import PageHero from "../../components/PageHero";
 import AppForm from "@travelia/components/Form";
 import FormikTextField from "@travelia/components/Inputs/TextField/FormikTextField";
+import toast from "react-hot-toast";
 import { contactSchema } from "@travelia/schemas/conact";
+import { emailPublicKey, emailServiceId, emailTemplateId } from "./config";
 
 type ContactFormValues = {
   user_email: string;
@@ -16,7 +20,25 @@ const initialValues: ContactFormValues = {
 };
 
 const Contact = () => {
-  const handleSubmit = async (values: ContactFormValues) => {};
+  const handleSubmit = async (
+    values: ContactFormValues,
+    { resetForm }: FormikHelpers<ContactFormValues>,
+  ) => {
+    const data = {
+      from_name: values.user_email,
+      from: values.user_email,
+      message: values.message,
+      to_name: "Dalia",
+    };
+    try {
+      await emailjs.send(emailServiceId, emailTemplateId, data, emailPublicKey);
+      resetForm();
+      toast.success("Message sent successfully!");
+    } catch (error) {
+      console.error("Failed to send message:", error);
+      toast.error("Failed to send message. Please try again later.");
+    }
+  };
 
   return (
     <>
